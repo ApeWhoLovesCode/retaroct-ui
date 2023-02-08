fef8d42ba05df1801930fe0fa9aebec2e830ae06
lhh <970519495@qq.com>
2023-02-08 18:34:11 +0800

    fix-解决taro过高变量导致的报错问题

--- .umirc.ts
+++ .umirc.ts
@@ -8,45 +8,18 @@ export default defineConfig({
   outputPath: 'docs-dist',
   mode: 'site',
   alias: {
-    // '@tarojs/taro': '@tarojs/taro-h5',
-    // '@tarojs/components$': '@tarojs/components/dist-h5/react',
+    '@tarojs/taro': '@tarojs/taro-h5',
+    '@tarojs/components$': '@tarojs/components/dist-h5/react',
   },
-  chainWebpack(memo, { webpack }) {
-    // memo.resolve.alias.set('@tarojs/taro', '@tarojs/taro-h5')
-    // memo.resolve.alias.set('@tarojs/components$', '@tarojs/components/dist-h5/react')
-
-    memo.merge({
-      resolve: {
-        alias: {
-          '@tarojs/taro': '@tarojs/taro-h5',
-          '@tarojs/components$': '@tarojs/components/dist-h5/react',
-        },
-      },
-      plugins: [
-        new webpack.DefinePlugin({
-          'process.env.TARO_ENV': JSON.stringify('h5'),
-          ENABLE_INNER_HTML: JSON.stringify(false),
-          ENABLE_ADJACENT_HTML: JSON.stringify(false),
-          ENABLE_SIZE_APIS: JSON.stringify(false),
-          ENABLE_TEMPLATE_CONTENT: JSON.stringify(false),
-          ENABLE_CLONE_NODE: JSON.stringify(false),
-          ENABLE_CONTAINS: JSON.stringify(false),
-          ENABLE_MUTATION_OBSERVER: JSON.stringify(false),
-        }),
-      ].filter(Boolean),
-    });
-    // memo.plugins().add(
-    //   new webpack.DefinePlugin({
-    //     'process.env.TARO_ENV': JSON.stringify('h5'),
-    //     ENABLE_INNER_HTML: JSON.stringify(false),
-    //     ENABLE_ADJACENT_HTML: JSON.stringify(false),
-    //     ENABLE_SIZE_APIS: JSON.stringify(false),
-    //     ENABLE_TEMPLATE_CONTENT: JSON.stringify(false),
-    //     ENABLE_CLONE_NODE: JSON.stringify(false),
-    //     ENABLE_CONTAINS: JSON.stringify(false),
-    //     ENABLE_MUTATION_OBSERVER: JSON.stringify(false),
-    //   })
-    // )
+  define: {
+    'process.env.TARO_ENV': 'h5',
+    ENABLE_INNER_HTML: 'false',
+    ENABLE_ADJACENT_HTML: 'false',
+    ENABLE_SIZE_APIS: 'false',
+    ENABLE_TEMPLATE_CONTENT: 'false',
+    ENABLE_CLONE_NODE: 'false',
+    ENABLE_CONTAINS: 'false',
+    ENABLE_MUTATION_OBSERVER: 'false',
   },
   // more config: https://d.umijs.org/config
 });
