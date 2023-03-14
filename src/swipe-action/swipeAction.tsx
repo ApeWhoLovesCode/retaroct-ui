e5829850185c0cacb27c0c76ebb53c327d5d0b12
lhh <970519495@qq.com>
2023-03-14 18:29:05 +0800

    fix-swipe-action改用useTouchEvent钩子来触发触摸事件

--- src/swipe-action/swipeAction.tsx
+++ src/swipe-action/swipeAction.tsx
@@ -1,19 +1,12 @@
 import { View } from '@tarojs/components';
-import React, {
-  useState,
-  useEffect,
-  forwardRef,
-  useRef,
-  useImperativeHandle,
-  useCallback,
-} from 'react';
+import React, { useState, useEffect, forwardRef, useRef, useImperativeHandle } from 'react';
 import { withNativeProps } from '../utils/native-props';
 import getEleInfo from '../utils/getEleInfo';
-import useTouch from '../use-touch';
 import { nextTick } from '@tarojs/taro';
 import { randomStr } from '../utils/random';
-import { isMobile, MouseTouchEvent } from '../utils/handleDom';
+import { MouseTouchEvent } from '../utils/handleDom';
 import { SwipeActionProps, SwipeActionRef, SwipeActionType } from './type';
+import useTouchEvent from '../use-touch-event';
 
 const classPrefix = 'retaroct-slide-action';
 /** 用来控制关闭其他的滑块 */
@@ -62,7 +55,8 @@ const SlideAction = forwardRef<SwipeActionRef, SwipeActionProps>(
       left: 0,
       right: 0,
     });
-    const touch = useTouch();
+    /** 动画时间 */
+    const duration = useRef(0.4);
 
     const close = () => {
       if (touchRef.current.direction !== 'normal') {
@@ -71,70 +65,61 @@ const SlideAction = forwardRef<SwipeActionRef, SwipeActionProps>(
       }
     };
 
-    const onTouchStart = (e: MouseTouchEvent) => {
-      e.stopPropagation();
-      touch.start(e);
-      touchRef.current.status = 'not';
-      if (!isMobile()) {
-        document.addEventListener('mousemove', onTouchMove, true);
-        document.addEventListener('mouseup', onTouchEnd, true);
-      }
-    };
-    const onTouchMove = useCallback((e: MouseTouchEvent) => {
-      e.stopPropagation();
-      if (touchRef.current.status === 'lock') return;
-      touch.move(e);
-      if (touchRef.current.status !== 'active') {
-        // 当前垂直方向滚动大于10px
-        if (touch.info.offsetY > 10) {
-          touchRef.current.status = 'lock';
-          return;
+    const { info, onTouchFn } = useTouchEvent({
+      onTouchStart: () => {
+        touchRef.current.status = 'not';
+        duration.current = 0.1;
+      },
+      onTouchMove: () => {
+        if (touchRef.current.status === 'lock') return;
+        if (touchRef.current.status !== 'active') {
+          // 当前垂直方向滚动大于10px
+          if (info.offsetY > 10) {
+            touchRef.current.status = 'lock';
+            return;
+          }
+          // 触摸时间小于100ms
+          if (info.time < 100) return;
+          touchRef.current.status = 'active';
         }
-        // 触摸时间小于100ms
-        if (touch.info.time < 100) return;
-        touchRef.current.status = 'active';
-      }
-      const x = touch.info.deltaX;
-      const rangeV = -1 * areaRef.current[x < 0 ? 'right' : 'left'];
-      // 滑块发生了位移的值
-      const directionV = areaRef.current[`${touchRef.current.direction}`] ?? 0;
-      const _x = Math[x < 0 ? 'max' : 'min'](x - directionV, rangeV);
-      setMoveX(_x);
-      Object.keys(controller).forEach((key) => {
-        if (key !== idRef.current && controller[key].closeOnTouchOutside) {
-          controller[key]?.close();
+        const x = info.deltaX;
+        const rangeV = -1 * areaRef.current[x < 0 ? 'right' : 'left'];
+        // 滑块发生了位移的值
+        const directionV = areaRef.current[`${touchRef.current.direction}`] ?? 0;
+        const _x = Math[x < 0 ? 'max' : 'min'](x - directionV, rangeV);
+        setMoveX(_x);
+        Object.keys(controller).forEach((key) => {
+          if (key !== idRef.current && controller[key].closeOnTouchOutside) {
+            controller[key]?.close();
+          }
+        });
+      },
+      onTouchEnd: (e: MouseTouchEvent) => {
+        e.stopPropagation();
+        if (touchRef.current.status === 'lock') return;
+        const x = info.deltaX;
+        const direction = x > 0 ? 'left' : 'right';
+        const rangeV = areaRef.current[direction];
+        const directionV = areaRef.current[`${touchRef.current.direction}`] ?? 0;
+        let _moveX = -directionV;
+        // 触摸大于(一半区域 + 按钮显示后的宽度)
+        if (Math.abs(x) >= Math.abs(rangeV) / 2 + Math.abs(directionV)) {
+          touchRef.current.direction = direction;
+          _moveX = -rangeV;
+        } else {
+          // 还没有触发按钮显示，则正常触发，当按钮显示后需要触摸大于一半区域
+          if (!directionV || Math.abs(x) > Math.abs(directionV) / 2) {
+            touchRef.current.direction = 'normal';
+            _moveX = 0;
+          }
         }
-      });
-    }, []);
-    const onTouchEnd = useCallback((e: MouseTouchEvent) => {
-      e.stopPropagation();
-      if (touchRef.current.status === 'lock') return;
-      if (!isMobile()) {
-        document.removeEventListener('mousemove', onTouchMove, true);
-        document.removeEventListener('mouseup', onTouchEnd, true);
-      }
-      touch.move(e);
-      const x = touch.info.deltaX;
-      const direction = x > 0 ? 'left' : 'right';
-      const rangeV = areaRef.current[direction];
-      const directionV = areaRef.current[`${touchRef.current.direction}`] ?? 0;
-      let _moveX = -directionV;
-      // 触摸大于(一半区域 + 按钮显示后的宽度)
-      if (Math.abs(x) >= Math.abs(rangeV) / 2 + Math.abs(directionV)) {
-        touchRef.current.direction = direction;
-        _moveX = -rangeV;
-      } else {
-        // 还没有触发按钮显示，则正常触发，当按钮显示后需要触摸大于一半区域
-        if (!directionV || Math.abs(x) > Math.abs(directionV) / 2) {
-          touchRef.current.direction = 'normal';
-          _moveX = 0;
+        duration.current = 0.4;
+        setMoveX(_moveX);
+        if (_moveX !== 0) {
+          onActionsReveal?.(direction);
         }
-      }
-      setMoveX(_moveX);
-      if (_moveX !== 0) {
-        onActionsReveal?.(direction);
-      }
-    }, []);
+      },
+    });
 
     useImperativeHandle(ref, () => ({
       show: (side: 'left' | 'right' = 'right') => {
@@ -187,22 +172,6 @@ const SlideAction = forwardRef<SwipeActionRef, SwipeActionProps>(
       );
     };
 
-    const handleEvent = () => {
-      if (!isMobile()) {
-        return {
-          onMouseDown: onTouchStart,
-          onMouseUp: onTouchEnd,
-        };
-      } else {
-        return {
-          onTouchStart: onTouchStart,
-          onTouchMove: onTouchMove,
-          onTouchEnd: onTouchEnd,
-          onTouchCancel: onTouchEnd,
-        };
-      }
-    };
-
     return withNativeProps(
       ret,
       <View className={`${classPrefix} ${idRef.current}`}>
@@ -210,8 +179,11 @@ const SlideAction = forwardRef<SwipeActionRef, SwipeActionProps>(
           className={`${classPrefix}-content`}
           // 添加该属性后，滑动盒子是不会触发滚动效果的 // 不太符合这里，会导致很少区域才能触发滚动
           // catchMove={true}
-          style={{ transform: `translateX(${moveX}px)` }}
-          {...handleEvent()}
+          style={{
+            transitionDuration: duration.current + 's',
+            transform: `translateX(${moveX}px)`,
+          }}
+          {...onTouchFn}
         >
           <View className={`${classPrefix}-left ${classPrefix}-area`}>
             {leftActions?.map(renderButton)}
